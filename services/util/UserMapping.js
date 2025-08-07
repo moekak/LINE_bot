2bfaebcd602daebe45f51a7a776b75acb5c8d3d5
moekak <moeka.0729@gmail.com>
2025-08-07 19:04:02 +0900

    流入アクション

--- services/util/UserMapping.js
+++ services/util/UserMapping.js
@@ -1,34 +1,34 @@
 const { default: axios } = require("axios");
 
 class UserMapping {
-    constructor(channelAccessToken, targetUserId) {
-        this.channelAccessToken = channelAccessToken;
-        this.targetUserId = targetUserId;
-    }
+      constructor(channelAccessToken, targetUserId) {
+            this.channelAccessToken = channelAccessToken;
+            this.targetUserId = targetUserId;
+      }
 
-    async generateLinkToken() {
-        console.log("linkToken取得開始");
-        
-        try {
-            const response = await axios.post(
-                `https://api.line.me/v2/bot/user/${this.targetUserId}/linkToken`,
-                {},
-                {
-                    headers: {
-                        Authorization: `Bearer ${this.channelAccessToken}`,
-                    },
-                }
-            );
-
-            const linkToken = response.data.linkToken;
-            console.log("linkToken:", linkToken);
-            return linkToken;
+      async generateLinkToken() {
+            console.log("linkToken取得開始");
             
-        } catch (error) {
-            console.error('Error fetching link token:', error.response?.data || error.message);
-            throw error;
-        }
-    }
+            try {
+                  const response = await axios.post(
+                  `https://api.line.me/v2/bot/user/${this.targetUserId}/linkToken`,
+                  {},
+                        {
+                              headers: {
+                                    Authorization: `Bearer ${this.channelAccessToken}`,
+                              },
+                        }
+                  );
+
+                  const linkToken = response.data.linkToken;
+                  console.log("linkToken:", linkToken);
+                  return linkToken;
+                  
+            } catch (error) {
+                  console.error('Error fetching link token:', error.response?.data || error.message);
+                  throw error;
+            }
+      }
 }
 
 module.exports = UserMapping;
